6966eeaa0f35d735d354335968374df44fe0c348
eli-h <eli.heineman@gmail.com>
2017-07-31 21:18:47 +0000

    built downloader

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,42 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
 var GITHUB_USER = "eli-h";
 var GITHUB_TOKEN = "69b35fc62c9049c0a4b2122120f26da7a4cccf45";
+
 function getRepoContributers(repoOwner, repoName, cb){
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  //var parse = {};
+  var requestObj = {
+  	url: requestURL,
+    headers: {'User-Agent': 'GitHub Avatar Downloader - Student Project'}
+  };
+
+  request.get(requestObj, function(error, response, body){
+  	 var parsed = JSON.parse(body);
+  	 cb(error, parsed);
+  });
 }
 
-getRepoContributers("jquery", "jquery", function(err, result){
-	console.log(err);
-	console.log(result);
+var picLinks = getRepoContributers("jquery", "jquery", function(err, result){
+	var links = ""
+	for (var key in result){
+		links += (result[key].avatar_url + '\n')
+	}
+	return(links)
 });
+
+function downloadImageByUrl(url, filePath){
+  request.get(url)
+         .on('error', function(err){
+         	throw err;
+         })
+         .on('response', function(response){
+         	console.log(response.statusCode);
+         })
+         .pipe(fs.createWriteStream(filePath));
+}
+
+//downloadImageByUrl("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./downloads/pic.jpg");
