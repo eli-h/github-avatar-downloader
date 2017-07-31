0bf1121750f9c4bbec1d15c50d8320e45e35dbac
eli-h <eli.heineman@gmail.com>
2017-07-31 19:52:19 +0000

    can acces the object

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,14 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+var GITHUB_USER = "eli-h";
+var GITHUB_TOKEN = "69b35fc62c9049c0a4b2122120f26da7a4cccf45";
+function getRepoContributers(repoOwner, repoName, cb){
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+
+getRepoContributers("jquery", "jquery", function(err, result){
+	console.log(err);
+	console.log(result);
+});
